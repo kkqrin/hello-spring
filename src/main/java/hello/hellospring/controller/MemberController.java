e728ba89e828a9865f7e65762451c9d6e0fed135
kkqrin <126125426+kkqrin@users.noreply.github.com>
2023-07-06 20:44:48 +0900

    회원 웹 기능 - 회원가입

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,18 +1,35 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
+    // DI 1. 필드 주입 2. setter 주입 (수정될 수 있음) 3. 생성자 주입 (권장)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
-    // DI 1. 필드 주입 2. setter 주입 (수정될 수 있음) 3. 생성자 주입 (권장)
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
